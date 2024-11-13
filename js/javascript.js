5b472012cbece8bb5217aeb12ec2258937f3ea54
Zsolt <184605676+Zsolt-qwerty@users.noreply.github.com>
2024-11-13 23:53:55 +0000

    "About Us" button revamp
    Complete redesign what the "About Us" button does, now it prints next to
    the button and can hide it once user finished with it.

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,15 @@
+let toggle = "on";
+
 function AboutUs() {
-    alert("About Us: \n\nWe are passionate about cooking and sharing recipes with like-minded people.");
+    if (toggle == "on") {
+/*        alert("About Us: \n\nWe are passionate about cooking and sharing recipes with like-minded people.");
+*/
+        document.getElementById("AboutUsText").innerHTML = " | We are passionate about cooking and sharing recipes with like-minded people.";
+        document.getElementById("AboutUsButton").innerHTML = "...hide...";
+        toggle = "off";
+    } else {
+        document.getElementById("AboutUsText").innerHTML = " ";
+        document.getElementById("AboutUsButton").innerHTML = "About Us";
+        toggle = "on";
+    }
 }
